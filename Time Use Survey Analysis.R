aebb9813f9f5a07665f9e5426ad36bca5d99c1fb
briangillis <brian.gillis@seiu.org>
2017-02-24 13:35:43 -0500

    small change to test
    just changed some spacing

--- Time Use Survey Analysis.R
+++ Time Use Survey Analysis.R
@@ -16,9 +16,6 @@ pg <- list(a, b, c)
 
 # Loop through the sheets and load the particular data
 
-
-
-
 for (j in pg)  {
   
   sheets <- c("2004","2005", "2006","2007", "2008","2009", "2010", "2011","2012","2013","2014","2015")
